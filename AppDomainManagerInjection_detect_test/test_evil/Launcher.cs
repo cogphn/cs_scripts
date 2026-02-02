5c1d0d4f982526943fcc010160d0af18dff06623
cogphn <jason.deyalsingh@gmail.com>
2026-02-01 22:27:23 -0500

    Create Launcher.cs

---
+++ AppDomainManagerInjection_detect_test/test_evil/Launcher.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Launcher
+{
+	// Token: 0x02000002 RID: 2
+	internal class Program
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Starting...");
+			long num = 9223372036824775807L;
+			long num2 = num / 3L;
+			Console.WriteLine("Done.");
+		}
+	}
+}
